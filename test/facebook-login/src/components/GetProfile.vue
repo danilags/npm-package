b50330f6ef2cd900d02bb36d0c610b3a2eef0e4f
hacktiv8 <aguspermadi94@gmail.com>
2017-04-18 13:59:41 +0700

    half done

---
+++ test/facebook-login/src/components/GetProfile.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <div class="">
+    <p>Name : {{  }}</p>
+    <p>Email :{{ }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ test/facebook-login/src/components/LoginUser.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <div class="">
+    <a href="#"><h3>login</h3></a>
+
+    <!-- <div class="fb-login-button" data-max-rows="1" data-size="medium" data-show-faces="false" data-auto-logout-link="false"></div> -->
+  </div>
+</template>
+cd .
+<script>
+
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
